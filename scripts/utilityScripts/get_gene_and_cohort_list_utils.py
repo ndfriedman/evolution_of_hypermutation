1dbe7af0fc5147465408a971f4fb7898365a8e1b
ndfriedman <mail4noahf@gmail.com>
2020-10-02 19:58:35 -0700

    changes

--- scripts/utilityScripts/get_gene_and_cohort_list_utils.py
+++ scripts/utilityScripts/get_gene_and_cohort_list_utils.py
@@ -42,29 +42,30 @@ def get_pathway_genes(pathway):
 	df = pd.read_csv(pathwayFileMap[pathway])
 	return set(df['Gene'])
 
-def set_up_dep_map_data(depMapPath = 'files/infoFiles/projectAchillesDepMapData.csv', cancerTypeLines = [], loadedDf = pd.DataFrame([{}])):
+
+def set_up_dep_map_data(depMapPath = '~/Desktop/hypermutationProjectFinal/files/infoFiles/projectAchillesDepMapData.csv', cancerTypeLines = [], loadedDf = pd.DataFrame([{}])):
 	
 	#if we are iterating over this function we have the option to only load the dataframe once
 	depMapData = None
-	if loadedDf.shape[0] > 0:
+	if loadedDf.shape[0] > 1:
 		depMapData = loadedDf
 	else:
 		depMapData = pd.read_csv(depMapPath)
 
 	if len(cancerTypeLines) > 0:
-		depMapData = depMapData[depMapData['Unnamed: 0'].isin(cancerTypeLines)]
+		depMapData = depMapData[depMapData['DepMap_ID'].isin(cancerTypeLines)]
 
 	listOfDicts = []
 	for col in depMapData.columns.values:
 	    gene = col.split(' ')[0]
-	    if col != 'Unnamed: 0':
+	    if col != 'DepMap_ID':
 	        listOfDicts.append({'Hugo_Symbol': gene, 'score': np.nanmean(depMapData[col])})
 	depMapDf = pd.DataFrame(listOfDicts)
 	return depMapDf
 
 #two modes: getEssentialGenes, and getAllGenes which just returns the df of scores
-def get_essential_genes(depMapPath = 'files/infoFiles/projectAchillesDepMapData.csv', mode='getEssentialGenes', cancerTypeLines = []):
-	depMapDf = load_dep_map_data(depMapPath = depMapPath, cancerTypeLines = cancerTypeLines)
+def get_essential_genes(depMapPath = '~/Desktop/hypermutationProjectFinal/files/infoFiles/projectAchillesDepMapData.csv', mode='getEssentialGenes', cancerTypeLines = []):
+	depMapDf = set_up_dep_map_data(depMapPath = depMapPath, cancerTypeLines = cancerTypeLines)
 
 	if mode == 'getEssentialGenes':
 		essentialGenes = set(depMapDf[depMapDf['score'] <= -1]['Hugo_Symbol'])
@@ -78,9 +79,9 @@ def get_essential_genes(depMapPath = 'files/infoFiles/projectAchillesDepMapData.
 		print 'error invalid mode specified'
 
 #returns all genes that have dep map scores >.5 and are not in impact 468
-def get_cancer_neutral_genes(depMapPath = 'files/infoFiles/projectAchillesDepMapData.csv', cancerTypeLines = []):
+def get_cancer_neutral_genes(depMapPath = '~/Desktop/hypermutationProjectFinal/files/infoFiles/projectAchillesDepMapData.csv', cancerTypeLines = []):
 	
-	depMapDf = load_dep_map_data(depMapPath = depMapPath, cancerTypeLines = cancerTypeLines)
+	depMapDf = set_up_dep_map_data(depMapPath = depMapPath, cancerTypeLines = cancerTypeLines)
 
 	impactGenes = set(['ABL1', 'ACVR1', 'AGO2', 'AKT1', 'AKT2', 'AKT3', 'ALK', 'ALOX12B', 'ANKRD11', 'APC', 'AR', 'ARAF', 'ARID1A', 'ARID1B', 'ARID2', 'ARID5B', 'ASXL1', 'ASXL2', 'ATM', 'ATR', 'ATRX', 'AURKA', 'AURKB', 'AXIN1', 'AXIN2', 'AXL', 'B2M', 'BABAM1', 'BAP1', 'BARD1', 'BBC3', 'BCL10', 'BCL2', 'BCL2L1', 'BCL2L11', 'BCL6', 'BCOR', 'BIRC3', 'BLM', 'BMPR1A', 'BRAF', 'BRCA1', 'BRCA2', 'BRD4', 'BRIP1', 'BTK', 'CALR', 'CARD11', 'CARM1', 'CASP8', 'CBFB', 'CBL', 'CCND1', 'CCND2', 'CCND3', 'CCNE1', 'CD274', 'CD276', 'CD79A', 'CD79B', 'CDC42', 'CDC73', 'CDH1', 'CDK12', 'CDK4', 'CDK6', 'CDK8', 'CDKN1A', 'CDKN1B', 'CDKN2A', 'CDKN2B', 'CDKN2C', 'CEBPA', 'CENPA', 'CHEK1', 'CHEK2', 'CIC', 'CREBBP', 'CRKL', 'CRLF2', 'CSDE1', 'CSF1R', 'CSF3R', 'CTCF', 'CTLA4', 'CTNNB1', 'CUL3', 'CXCR4', 'CYLD', 'CYSLTR2', 'DAXX', 'DCUN1D1', 'DDR2', 'DICER1', 'DIS3', 'DNAJB1', 'DNMT1', 'DNMT3A', 'DNMT3B', 'DOT1L', 'DROSHA', 'DUSP4', 'E2F3', 'EED', 'EGFL7', 'EGFR', 'EIF1AX', 'EIF4A2', 'EIF4E', 'ELF3', 'EP300', 'EPAS1', 'EPCAM', 'EPHA3', 'EPHA5', 'EPHA7', 'EPHB1', 'ERBB2', 'ERBB3', 'ERBB4', 'ERCC2', 'ERCC3', 'ERCC4', 'ERCC5', 'ERF', 'ERG', 'ERRFI1', 'ESR1', 'ETV1', 'ETV6', 'EZH1', 'EZH2', 'FAM123B', 'FAM175A', 'FAM46C', 'FAM58A', 'FANCA', 'FANCC', 'FAT1', 'FBXW7', 'FGF19', 'FGF3', 'FGF4', 'FGFR1', 'FGFR2', 'FGFR3', 'FGFR4', 'FH', 'FLCN', 'FLT1', 'FLT3', 'FLT4', 'FOXA1', 'FOXL2', 'FOXO1', 'FOXP1', 'FUBP1', 'FYN', 'GATA1', 'GATA2', 'GATA3', 'GLI1', 'GNA11', 'GNAQ', 'GNAS', 'GPS2', 'GREM1', 'GRIN2A', 'GSK3B', 'H3F3A', 'H3F3B', 'H3F3C', 'HGF', 'HIST1H1C', 'HIST1H2BD', 'HIST1H3A', 'HIST1H3B', 'HIST1H3C', 'HIST1H3D', 'HIST1H3E', 'HIST1H3F', 'HIST1H3G', 'HIST1H3H', 'HIST1H3I', 'HIST1H3J', 'HIST2H3C', 'HIST2H3D', 'HIST3H3', 'HLA-A', 'HLA-B', 'HNF1A', 'HOXB13', 'HRAS', 'ICOSLG', 'ID3', 'IDH1', 'IDH2', 'IFNGR1', 'IGF1', 'IGF1R', 'IGF2', 'IKBKE', 'IKZF1', 'IL10', 'IL7R', 'INHA', 'INHBA', 'INPP4A', 'INPP4B', 'INPPL1', 'INSR', 'IRF4', 'IRS1', 'IRS2', 'JAK1', 'JAK2', 'JAK3', 'JUN', 'KDM5A', 'KDM5C', 'KDM6A', 'KDR', 'KEAP1', 'KIT', 'KLF4', 'KMT2B', 'KMT5A', 'KNSTRN', 'KRAS', 'LATS1', 'LATS2', 'LMO1', 'LYN', 'MALT1', 'MAP2K1', 'MAP2K2', 'MAP2K4', 'MAP3K1', 'MAP3K13', 'MAP3K14', 'MAPK1', 'MAPK3', 'MAPKAP1', 'MAX', 'MCL1', 'MDC1', 'MDM2', 'MDM4', 'MED12', 'MEF2B', 'MEN1', 'MET', 'MGA', 'MITF', 'MLH1', 'KMT2A', 'KMT2B', 'KMT2C', 'MPL', 'MRE11A', 'MSH2', 'MSH3', 'MSH6', 'MSI1', 'MSI2', 'MST1', 'MST1R', 'MTOR', 'MUTYH', 'MYC', 'MYCL1', 'MYCN', 'MYD88', 'MYOD1', 'NBN', 'NCOA3', 'NCOR1', 'NEGR1', 'NF1', 'NF2', 'NFE2L2', 'NFKBIA', 'NKX2-1', 'NKX3-1', 'NOTCH1', 'NOTCH2', 'NOTCH3', 'NOTCH4', 'NPM1', 'NRAS', 'NSD1', 'NTHL1', 'NTRK1', 'NTRK2', 'NTRK3', 'NUF2', 'NUP93', 'PAK1', 'PAK7', 'PALB2', 'PARK2', 'PARP1', 'PAX5', 'PBRM1', 'PDCD1', 'PDCD1LG2', 'PDGFRA', 'PDGFRB', 'PDPK1', 'PGR', 'PHOX2B', 'PIK3C2G', 'PIK3C3', 'PIK3CA', 'PIK3CB', 'PIK3CD', 'PIK3CG', 'PIK3R1', 'PIK3R2', 'PIK3R3', 'PIM1', 'PLCG2', 'PLK2', 'PMAIP1', 'PMS1', 'PMS2', 'PNRC1', 'POLD1', 'POLE', 'PPARG', 'PPM1D', 'PPP2R1A', 'PPP4R2', 'PPP6C', 'PRDM1', 'PRDM14', 'PREX2', 'PRKAR1A', 'PRKCI', 'PRKD1', 'PTCH1', 'PTEN', 'PTP4A1', 'PTPN11', 'PTPRD', 'PTPRS', 'PTPRT', 'RAB35', 'RAC1', 'RAC2', 'RAD21', 'RAD50', 'RAD51', 'RAD51C', 'RAD51L1', 'RAD51L3', 'RAD52', 'RAD54L', 'RAF1', 'RARA', 'RASA1', 'RB1', 'RBM10', 'RECQL', 'RECQL4', 'REL', 'RET', 'RFWD2', 'RHEB', 'RHOA', 'RICTOR', 'RIT1', 'RNF43', 'ROS1', 'RPS6KA4', 'RPS6KB2', 'RPTOR', 'RRAGC', 'RRAS', 'RRAS2', 'RTEL1', 'RUNX1', 'RXRA', 'RYBP', 'SDHA', 'SDHAF2', 'SDHB', 'SDHC', 'SDHD', 'SESN1', 'SESN2', 'SESN3', 'SETD2', 'SF3B1', 'SH2B3', 'SH2D1A', 'SHOC2', 'SHQ1', 'SLX4', 'SMAD2', 'SMAD3', 'SMAD4', 'SMARCA4', 'SMARCB1', 'SMARCD1', 'SMO', 'SMYD3', 'SOCS1', 'SOS1', 'SOX17', 'SOX2', 'SOX9', 'SPEN', 'SPOP', 'SPRED1', 'SRC', 'SRSF2', 'STAG2', 'STAT3', 'STAT5A', 'STAT5B', 'STK11', 'STK19', 'STK40', 'SUFU', 'SUZ12', 'SYK', 'TAP1', 'TAP2', 'TBX3', 'TCEB1', 'TCF3', 'TCF7L2', 'TEK', 'TERT', 'TET1', 'TET2', 'TGFBR1', 'TGFBR2', 'TMEM127', 'TMPRSS2', 'TNFAIP3', 'TNFRSF14', 'TOP1', 'TP53', 'TP53BP1', 'TP63', 'TRAF2', 'TRAF7', 'TSC1', 'TSC2', 'TSHR', 'U2AF1', 'UPF1', 'VEGFA', 'VHL', 'VTCN1', 'WHSC1', 'WHSC1L1', 'WT1', 'WWTR1', 'XIAP', 'XPO1', 'XRCC2', 'YAP1', 'YES1', 'ZFHX3', 'ZRSR2'])
 	neutralGenes = set(depMapDf[depMapDf['score'] >= -.5]['Hugo_Symbol'])
@@ -108,6 +109,24 @@ def get_cancer_type_specific_dep_map_data(cancerTypes):
 		dictOfDepMapData[cancerType] = dict(zip(depMapDf['Hugo_Symbol'], depMapDf['score']))
 	return dictOfDepMapData
 
+def get_cancer_type_specific_essential_genes(cancerTypes, essentialThresh = -1):
+	dictOfDepMapData = get_cancer_type_specific_dep_map_data(cancerTypes)
+	d = {}
+	for cancerType, cTypeDict in dictOfDepMapData.items():
+		l = [key for key, value in cTypeDict.items() if value < essentialThresh]
+		d[cancerType] = l
+	return d
+
+def get_expressed_genes_from_gtex(gtexDataPath = '', thresh=1):
+	gtexData = pd.read_table(gtexDataPath, skiprows=[0,1])
+	d = {}
+
+	cancerTypesGtex = set(gtexData.columns.values) - set(['Name', 'Description'])
+	print cancerTypesGtex
+	for cancerType in cancerTypesGtex:
+		expressedGenes = set(gtexData[gtexData[cancerType] > thresh]['Description'])
+		d[cancerType] = expressedGenes
+	return d
 
 #######
 ###########
@@ -149,7 +168,9 @@ def get_exome_signature_cohorts(tcgaSigPath = '', exomeSigPath = ''):
 	exomeRecaptureSignatures['dominantSignature'] = exomeRecaptureSignatures.apply(lambda row: dominant_signature_lambda_function(row, signatureColumns), axis=1)
 	tcgaSignatures['dominantSignature'] = tcgaSignatures.apply(lambda row: dominant_signature_lambda_function(row, signatureColumns), axis=1)
 
-	return dict(zip(exomeRecaptureSignatures['Sample Name'], exomeRecaptureSignatures['dominantSignature'])), dict(zip(tcgaSignatures['Sample Name'], tcgaSignatures['dominantSignature']))
+	tcgaSignatures['Tumor_Sample_Barcode'] = tcgaSignatures['Sample Name'].apply(lambda x: x[:15])
+
+	return dict(zip(exomeRecaptureSignatures['Sample Name'], exomeRecaptureSignatures['dominantSignature'])), dict(zip(tcgaSignatures['Tumor_Sample_Barcode'], tcgaSignatures['dominantSignature']))
 
 nmutAttributionThresh = 400
 def get_tcga_pole_mmr_hypermutator_ids(tcgaSigsPath = 'files/infoFiles/tcgaSigDecomposition.txt'):
