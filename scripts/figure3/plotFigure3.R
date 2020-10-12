2a9d2172d39917883d6fd0875ef798f1b32c44fa
ndfriedman <mail4noahf@gmail.com>
2020-10-12 15:51:42 -0700

    added plotting script for possible figure 3 figures

--- scripts/figure3/plotFigure3.R
+++ scripts/figure3/plotFigure3.R
@@ -16,6 +16,11 @@ emptyTheme <- theme(axis.line = element_blank(),
                     panel.grid.minor = element_blank(),
                     panel.border = element_blank(),
                     panel.background = element_blank())
+
+#adjust this as needed
+plottingDataPath = '/Users/friedman/Desktop/hypermutationProjectFinal/scripts/figure3/FIGURE3_PLOTTING_FILES/plotDataFiles/'
+plottingFilePath = '/Users/friedman/Desktop/hypermutationProjectFinal/scripts/figure3/FIGURE3_PLOTTING_FILES/figurePdfs/'
+
 #
 ######
 ###############
@@ -26,11 +31,40 @@ emptyTheme <- theme(axis.line = element_blank(),
 ######
 #
 
-#multiple scales: https://eliocamp.github.io/codigo-r/2018/09/multiple-color-and-fill-scales-with-ggplot2/
+#TRUNCATING MUTATIONS IN ESSENTIAL GENES
 
-#PLOT FIGURE 3A
+plot_figure_3z1 <- function(df){
+  p <- ggplot(df, aes(x=nNeutralTruncating))+
+    geom_smooth(aes(y=nEssentialTruncatingGenes, color='Truncating mutations in essential genes'))+
+    geom_smooth(aes(y=nEssentialDoubleTruncatingGenes, color='Double Truncating mutations in essential genes'))+
+    geom_smooth(aes(y=nNeutralDoubleTruncatingGenes, color='Double Truncating mutations in neutral genes'))+
+    theme_classic()+
+    ylab('n genes with truncating mutations')+
+    xlab('n truncating mutations')
+  return(p)
+}
 
-make_multiple_samples_plot <- function(df){
+#Figure 3z1
+figure3z1DataFrame <- read.table(paste(plottingDataPath, 'figure_3z1.tsv', sep=''), sep = '\t', header=TRUE)
+#We remove the top decile of most hypermutated 
+plt3z1 <- plot_figure_3z1(figure3z1DataFrame)
+saveFilePath = paste(plottingFilePath, 'figure3z1.pdf')
+ggsave(saveFilePath,
+       plot=plt3z1,  width = 5.5, height = 4, units = c("in"), limitsize = FALSE)
+
+#
+######
+###############
+########################
+##################################
+########################
+###############
+######
+#
+
+#SUMMARIZING CONTINUED TUMOR EVOLUTION PLOT
+
+plot_figure_3z2 <- function(df){
   p <- ggplot(df, aes(y=reorder(branchId, -1*orderingVal)))+
     
     #scale 1: cancer_type
@@ -76,35 +110,24 @@ make_multiple_samples_plot <- function(df){
           axis.text.x = element_text(angle=90), axis.text.y = element_blank())+
     
     scale_x_continuous(breaks= c(-0.5,.5,1.5, 2.25, 2.75, 3.25), labels=c('cancer type', 'truncal drivers', 'branch drivers', 'second hit TSG', 'de-novo biallelic TSG mutation', 'convergent evolution'), position = "top")
-    
-  return(p)
+  
+  legendCancerType <- get_legend(ggplot(df, aes(x=1, y=1, fill=cancerType))+ geom_tile()+ scale_fill_viridis_d())
+  legendSecondHitTSG <- get_legend(ggplot(df, aes(x=1, y=1, fill=secondHitTSG))+ geom_tile()+ scale_fill_manual(values = c('white', '#75816b')))
+  legendDenovoBiallelicTSG <- get_legend(ggplot(df, aes(x=1, y=1, fill=denovoBiallelicInactivation))+ geom_tile()+ scale_fill_manual(values = c('white', '#A87D7D')))
+  legendConvergentEvolution <- get_legend(ggplot(df, aes(x=1, y=1, fill=convergentEvolution))+ geom_tile()+ scale_fill_manual(values = c('white', '#4682b4')))
+  legendRelatedUnrelated <- get_legend(ggplot(df, aes(x=1,y=1, size=nRelatedBranch, colour='related')) +geom_point())
+  
+  combinedLegends <- plot_grid(legendCancerType, legendRelatedUnrelated, ncol=2)
+  combinedPlot <- plot_grid(p, ggplot(), combinedLegends, nrow=3, rel_heights = c(1, .05, .25))
+  return(combinedPlot)
 }
 
-df <- read.table('/Users/friedman/Desktop/WORK/dataForLocalPlotting/multipleSampleSummary.tsv', sep='\t', header=TRUE)
-p <- make_multiple_samples_plot(df)
-#Get legends for alignment
-legendCancerType <- get_legend(ggplot(df, aes(x=1, y=1, fill=cancerType))+ geom_tile()+ scale_fill_viridis_d())
-legendSecondHitTSG <- get_legend(ggplot(df, aes(x=1, y=1, fill=secondHitTSG))+ geom_tile()+ scale_fill_manual(values = c('white', '#75816b')))
-legendDenovoBiallelicTSG <- get_legend(ggplot(df, aes(x=1, y=1, fill=denovoBiallelicInactivation))+ geom_tile()+ scale_fill_manual(values = c('white', '#A87D7D')))
-legendConvergentEvolution <- get_legend(ggplot(df, aes(x=1, y=1, fill=convergentEvolution))+ geom_tile()+ scale_fill_manual(values = c('white', '#4682b4')))
-legendRelatedUnrelated <- get_legend(ggplot(df, aes(x=1,y=1, size=nRelatedBranch, colour='related')) +geom_point())
-
-combinedLegends <- plot_grid(legendCancerType, legendRelatedUnrelated, ncol=2)
-combinedPlot <- plot_grid(p, ggplot(), combinedLegends, nrow=3, rel_heights = c(1, .05, .25))
-ggsave('~/Desktop/plot.pdf', plot=combinedPlot,  width = 5, height = 10, units = c("in"))
-
-df$trunkId
-#
-######
-###############
-########################
-##################################
-########################
-###############
-######
-#
-#PLOT FIGURE 3B
-
+figure3z2DataFrame <- read.table(paste(plottingDataPath, 'figure_3z2.tsv', sep=''), sep = '\t', header=TRUE)
+#We remove the top decile of most hypermutated 
+plt3z2 <- plot_figure_3z2(figure3z2DataFrame)
+saveFilePath = paste(plottingFilePath, 'figure3z2.pdf')
+ggsave(saveFilePath,
+       plot=plt3z2,  width = 5, height = 10, units = c("in"), limitsize = FALSE)
 
 
 #
@@ -116,44 +139,45 @@ df$trunkId
 ###############
 ######
 #
-#PLOT FIGURE 3C
+#CLONALITY PLOT
 
-#Essential genes
-plot_essential_gene_mutagenesis <- function(df){
-  p <- ggplot(df)+
-      #geom_smooth(aes(x=score, y=nDoubleTruncating/(geneSize*nCancerType), colour='truncating'))+
-      #geom_smooth(aes(x=score, y=nDoubleMissense/(geneSize*nCancerType), colour='missense'))+
-      #geom_smooth(aes(x=score, y=nDoubleSilent/(geneSize*nCancerType), colour='silent'))
-    geom_histogram(x=score, y=nTruncating/(geneSize*nCancerType))
-    #geom_smooth(aes(x=score, y=nTruncating/(geneSize*nCancerType), colour='truncating'))+
-    #geom_smooth(aes(x=score, y=nMissense/(geneSize*nCancerType), colour='missense'))+
-    #geom_smooth(aes(x=score, y=nSilent/(geneSize*nCancerType), colour='silent'))
-  return(p)
+plot_figure_3z3 <- function(df){
+  p <- ggplot()+
+  
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'Endogenous') &
+                         ((df$Hugo_Symbol == 'POLE')),], aes(x=1, y=clonal), colour='#228B22')+
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'Endogenous') &
+                         ((df$Hugo_Symbol == 'MLH1') | (df$Hugo_Symbol == 'MSH2') | (df$Hugo_Symbol == 'MSH6')| (df$Hugo_Symbol == 'PMS2')),],
+               aes(x=2, y=clonal), colour='#228B22')+
+  
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'Endogenous'),], aes(x=3, y=clonal), colour='#228B22')+
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'Endogenous') & (df$geneType == 'TSG'),], aes(x=4, y=clonal), colour='#228B22')+
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'Endogenous') & (df$geneType == 'Oncogene'),], aes(x=5, y=clonal), colour='#228B22')+
+    stat_summary(data=df[(df$driverType == 'VUS') & (df$mutationType == 'Endogenous'),], aes(x=6, y=clonal), colour='#228B22')+
+  
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'TMZ-glioma'),], aes(x=7, y=clonal), colour='purple')+
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'TMZ-glioma') & (df$geneType == 'TSG'),], aes(x=8, y=clonal), colour='purple')+
+    stat_summary(data=df[(df$driverType == 'driver') & (df$mutationType == 'TMZ-glioma') & (df$geneType == 'Oncogene'),], aes(x=9, y=clonal), colour='purple')+
+    stat_summary(data=df[(df$driverType == 'VUS') & (df$mutationType == 'TMZ-glioma'),], aes(x=10, y=clonal), colour='purple')+
+  
+    ylim(0,1)+
+    ylab('Fraction of mutations clonal')+
+    scale_x_continuous(breaks=c(1,2,3,4,5,
+                              6,7,8,9,10), labels=c( 'POLE', 'MMR genes', 'All Drivers', 'TSG Drivers', 'Oncogene Drivers', 'All VUS',
+                                                     'All Drivers', 'TSG Drivers', 'Oncogene Drivers', 'All VUS'))+
+    theme(axis.text.x = element_text(angle=90))+
+    xlab('MMR/POLE tumors                TMZ Glioma')+
+    emptyTheme
+  return(p) 
 }
 
-df <- read.table('~/Desktop/WORK/dataForLocalPlotting/essentialGeneInfo.tsv', sep='\t', header=TRUE)
-p <- plot_essential_gene_mutagenesis(df)
-ggsave('~/Desktop/plot.pdf', plot=p,  width = 5, height = 5, units = c("in"))
 
-#
-######
-###############
-########################
-##################################
-########################
-###############
-######
-#
-#PLOT FIGURE 3D
+figure3z3DataFrame <- read.table(paste(plottingDataPath, 'figure_3z3.tsv', sep=''), sep = '\t', header=TRUE)
+plt3z3 <- plot_figure_3z3(figure3z3DataFrame)
+saveFilePath = paste(plottingFilePath, 'figure3z3.pdf')
+ggsave(saveFilePath,
+       plot=plt3z3,  width = 5, height = 10, units = c("in"), limitsize = FALSE)
 
-#
-######
-###############
-########################
-##################################
-########################
-###############
-######
-#
-#PLOT FIGURE 3E
+
+
 
